45953a588409b2e94e91761ed639c4af25eac5c0
thangnq <thangnq1001@gmail.com>
2021-06-03 00:34:13 +0700

    Add simple unit tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import assert from 'assert';
+import {calculateAge} from './utils/common';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+//  init stuff here
+});
+
+afterAll(() => {
+//  clean up stuff here
+});
+
+describe('utils/common', () => {
+  describe('calculateAge', () => {
+    it('should return correct result when year of birth is current year', () => {
+      expect(calculateAge(new Date().getFullYear())).toBe(1);
+    });
+    it('should return correct result when year of birth is a past year', () => {
+      expect(calculateAge(new Date().getFullYear() - 20)).toBe(21);
+    });
+    it('should return 0 when year of birth is a future year', () => {
+      expect(calculateAge(new Date().getFullYear() + 1)).toBe(0);
+    });
+    it('should return 0 when year of birth is null', () => {
+      expect(calculateAge(null)).toBe(0);
+    });
+    it('should return 0 when year of birth is undefined', () => {
+      expect(calculateAge()).toBe(0);
+    });
+  });
 });
--- frontend/src/utils/common.js
+++ frontend/src/utils/common.js
@@ -1,3 +1,13 @@
+// the rule of calculating age is simplified and defined by myself, but it actually depends on requirement of specific apps
+// in abnormal cases, it returns 0
+// assume yearOfBirth is always a number (we can use TypeScript for type checking)
 export const calculateAge = (yearOfBirth) => {
-  return new Date().getFullYear() - yearOfBirth;
+  if (yearOfBirth == null) {
+    return 0;
+  }
+  let age = new Date().getFullYear() - yearOfBirth + 1;
+  if (age <= 0) {
+    return 0;
+  }
+  return age;
 }
